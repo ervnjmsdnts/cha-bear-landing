42d2499e494eddac4696888b391670236210673f
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-05-14 14:25:49 +0800

    responsive

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
     <Section className='flex justify-center'>
       <div className='text-center'>
         <h1 className='font-bold text-7xl mb-8 text-black-dark'>HEADING</h1>
-        <p className='w-[600px] text-black-light mb-12'>
+        <p className='md:w-[600px] w-[300px] text-black-light mb-12'>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate,
           commodi dolorem similique inventore laborum asperiores quibusdam minus
           vitae id veritatis saepe! Vero soluta sed ipsum velit fugiat porro
